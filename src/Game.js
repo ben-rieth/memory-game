7b13348101d760a32ca0e7d8b78ce9e62b36fe4f
ben-rieth <benrieth3@gmail.com>
2022-06-11 20:16:59 -0500

    Add state variables to Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,13 @@
+import {useState} from 'react';
+
 import Card from './components/Card';
 
 function Game() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [level, setLevel] = useState(0);
+  const [lostGame, setLostGame] = useState(false);
+
   return (
     <div>
       <h1>Memory Game</h1>
