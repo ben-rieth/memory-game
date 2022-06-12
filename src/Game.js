ebc689260035593db7a43c0ee47fc8c9fb363770
ben-rieth <benrieth3@gmail.com>
2022-06-11 20:21:06 -0500

    Create methods to adjust game state

--- src/Game.js
+++ src/Game.js
@@ -8,6 +8,29 @@ function Game() {
   const [level, setLevel] = useState(0);
   const [lostGame, setLostGame] = useState(false);
 
+  const addPointToScore = () => {
+    setScore(score + 1);
+  }
+
+  const setNewBestScore = () => {
+    setBestScore(score);
+  }
+
+  const nextLevel = () => {
+    setLevel(level + 1);
+  }
+
+  const gameLost = () => {
+    setLostGame(true);
+    setNewBestScore();
+  }
+
+  const startNewGame = () => {
+    setLostGame(false);
+    setScore(0);
+    setLevel(1);
+  }
+
   return (
     <div>
       <h1>Memory Game</h1>
