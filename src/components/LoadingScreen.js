1eb9a892aeb7b78de009f1852ce087e009d5476e
ben-rieth <benrieth3@gmail.com>
2022-06-13 21:25:23 -0500

    Add loading spinner animation

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import LoadingSpinner from "./LoadingSpinner";
 
 const LoadingContainer = styled.div` 
     margin-top: 20vh;
@@ -7,6 +8,13 @@ const LoadingContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    gap: 30px;
+
+    & > h1 {
+        color: white;
+        font-family: "Spongebob";
+        letter-spacing: 3px;
+    }
 `;
 
 const LoadingScreen = (props) => {
@@ -15,6 +23,7 @@ const LoadingScreen = (props) => {
     return (
         <LoadingContainer>
             <h1>Loading Level {level}</h1> 
+            <LoadingSpinner />
         </LoadingContainer>
     );
 }
