c4ae88cdea910f6f0815654a072590058143bb57
ben-rieth <benrieth3@gmail.com>
2022-06-13 21:35:13 -0500

    Style game over screen

--- src/components/GameOverScreen.js
+++ src/components/GameOverScreen.js
@@ -4,9 +4,32 @@ const GameOverContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 10px;
+
+    & > h2 {
+        font-size: 2rem;
+    }
+
+    & > h2, button {
+        font-family: "Spongebob";
+        color: white;
+        letter-spacing: 3px;
+    }
 
     & > button {
-        
+        background-color: #26B9C8;
+        color: white;
+        border-radius: 10px;
+        padding: 15px 40px;
+        cursor: pointer;
+        font-size: 1.5rem;
+        border: none;
+
+        &:hover {
+            transform: scale(1.05);
+            background-color: white;
+            color: #26B9C8;
+        }
     }
 `;
 
